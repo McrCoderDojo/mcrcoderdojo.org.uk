808c2ef72c821bde599fce44623d4826ec524ed4
Ben Nuttall <ben@bennuttall.com>
2013-12-13 21:16:33 +0000

    Add Google+ link as meta link publisher

--- themes/mcrcoderdojo/header.php
+++ themes/mcrcoderdojo/header.php
@@ -24,6 +24,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="publisher" href="https://plus.google.com/117855294069328779199" />
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
