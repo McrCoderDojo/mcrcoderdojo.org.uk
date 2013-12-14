1f61668e5adcad6688a3e4483f8ed2563ebe25d0
Ben Nuttall <ben@bennuttall.com>
2013-12-14 01:12:37 +0000

    Add Venues post type

--- themes/mcrcoderdojo/coderdojo-functions.php
+++ themes/mcrcoderdojo/coderdojo-functions.php
@@ -37,3 +37,39 @@ function create_events_post_type() {
     register_post_type('event', $args);
 }
 add_action('init', 'create_events_post_type');
+
+function create_venues_post_type() {
+    $labels = array(
+        'name'               => 'Venues',
+        'singular_name'      => 'Venue',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Venue',
+        'edit_item'          => 'Edit Venue',
+        'new_item'           => 'New Venue',
+        'all_items'          => 'All Venues',
+        'view_item'          => 'View Venue',
+        'search_items'       => 'Search Venues',
+        'not_found'          => 'No Venues found',
+        'not_found_in_trash' => 'No Venues found in Trash',
+        'parent_item_colon'  => '',
+        'menu_name'          => 'Venues',
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => false,
+        'publicly_queryable' => false,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => null,
+        'capability_type'    => 'post',
+        'has_archive'        => false,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array('title'),
+    );
+
+    register_post_type('venue', $args);
+}
+add_action('init', 'create_venues_post_type');
