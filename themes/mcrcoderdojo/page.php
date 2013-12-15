34737b3f994865694752b09e562ecbb91757e2f4
Ben Nuttall <ben@bennuttall.com>
2013-12-15 13:06:03 +0000

    Cleanup

--- themes/mcrcoderdojo/page.php
+++ themes/mcrcoderdojo/page.php
@@ -4,7 +4,8 @@
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<?php while (have_posts()) : the_post();
+			<?php while (have_posts()):
+				the_post();
 				get_template_part('content', 'page');
 			endwhile; ?>
 
