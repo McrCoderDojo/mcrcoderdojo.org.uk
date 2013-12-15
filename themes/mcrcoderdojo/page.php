e9d47ebcf1b1bf7776ff28a7e95089c6044bb0ef
Ben Nuttall <ben@bennuttall.com>
2013-12-15 13:05:48 +0000

    Cleanup

--- themes/mcrcoderdojo/page.php
+++ themes/mcrcoderdojo/page.php
@@ -1,42 +1,16 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other 'pages' on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 <div id="main-content" class="main-content">
-
-<?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					// Include the page content template.
-					get_template_part( 'content', 'page' );
-
-				endwhile;
-			?>
+			<?php while (have_posts()) : the_post();
+				get_template_part('content', 'page');
+			endwhile; ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
+	<?php get_sidebar('content'); ?>
 </div><!-- #main-content -->
 
 <?php
