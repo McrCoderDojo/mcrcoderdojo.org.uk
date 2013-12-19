f11158604de2bb0ef64f8317c6961f2e01d2fe4a
Ben Nuttall <ben@bennuttall.com>
2013-12-19 14:56:12 +0000

    Show author name by blog posts in grid

--- themes/mcrcoderdojo/grid-item.php
+++ themes/mcrcoderdojo/grid-item.php
@@ -5,8 +5,16 @@
         <a href="<?php the_permalink(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/coderdojo.png" width="150" height="150" /></a>
     <?php endif;
 
+    $categories = get_the_category();
+    $categories = array_map(function($category) {return $category->slug;}, $categories);
+
     if (get_post_type() == 'event'): ?>
     <h2><a href="<?php the_permalink(); ?>"><?php echo date(get_option('date_format'), strtotime(get_field('date'))); ?></a></h2>
+    <?php $venue = mcd_get_venue_from_array(get_field('venue')); ?>
+    <span class="venue"><?php echo get_the_title($venue); ?></span>
+    <?php elseif (get_post_type() == 'post' && in_array('blog', $categories)): ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <span class="author"><?php the_author(); ?></span>
     <?php else: ?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <span class="date"><?php echo get_the_date(); ?></span>
