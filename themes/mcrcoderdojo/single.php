bd9022355bee0ecd8217a0c36fad80861289b4eb
Ben Nuttall <ben@bennuttall.com>
2013-12-19 12:37:08 +0000

    Cleanup

--- themes/mcrcoderdojo/single.php
+++ themes/mcrcoderdojo/single.php
@@ -1,37 +1,18 @@
-<?php
-/**
- * The Template for displaying all single posts
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-
-					get_template_part( 'content', get_post_type() );
-
-					// Previous/next post navigation.
+				while (have_posts()) {
+					the_post();
+					get_template_part('content', get_post_type());
 					twentyfourteen_post_nav(get_post_type());
-
-				endwhile;
+				}
 			?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar( 'content' );
+get_sidebar('content');
 get_sidebar();
 get_footer();
