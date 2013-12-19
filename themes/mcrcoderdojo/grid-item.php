fd60260b4ee15fb60aeeaecc0e7b724e81863562
Ben Nuttall <ben@bennuttall.com>
2013-12-19 20:37:43 +0000

    Show date by blog post items in grid, and show author name if not author page

--- themes/mcrcoderdojo/grid-item.php
+++ themes/mcrcoderdojo/grid-item.php
@@ -9,14 +9,19 @@
     $categories = array_map(function($category) {return $category->slug;}, $categories);
 
     if (get_post_type() == 'event'): ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php echo date(get_option('date_format'), strtotime(get_field('date'))); ?></a></h2>
-    <?php $venue = mcd_get_venue_from_array(get_field('venue')); ?>
-    <span class="venue"><?php echo get_the_title($venue); ?></span>
+        <h2><a href="<?php the_permalink(); ?>"><?php echo date(get_option('date_format'), strtotime(get_field('date'))); ?></a></h2>
+        <?php $venue = mcd_get_venue_from_array(get_field('venue')); ?>
+        <span class="venue"><?php echo get_the_title($venue); ?></span>
+
     <?php elseif (get_post_type() == 'post' && in_array('blog', $categories)): ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <span class="author"><?php the_author(); ?></span>
-    <?php else: ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <span class="date"><?php echo get_the_date(); ?></span>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <span class="date"><?php echo get_the_date(); ?></span>
+        <?php if (!is_author()): ?>
+            <br /><span class="author"><?php the_author(); ?></span>
+        <?php endif;
+
+    else: ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <span class="date"><?php echo get_the_date(); ?></span>
     <?php endif; ?>
 </div>
