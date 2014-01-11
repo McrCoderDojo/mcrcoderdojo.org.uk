a197f0b966c674019965eb5e1de7ffab5c61d9e3
Ben Nuttall <ben@bennuttall.com>
2014-01-11 23:02:34 +0000

    Add issue number and tagline to single view and newsletter template, fix #10 and #11

--- plugins/mcrcoderdojo-newsletter-generator/intro.php
+++ plugins/mcrcoderdojo-newsletter-generator/intro.php
@@ -6,8 +6,9 @@
                     <tbody>
                         <tr>
                             <td valign="top" class="mcnTextContent" style="padding-top: 9px;padding-right: 18px;padding-bottom: 9px;padding-left: 18px;border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;color: #606060;font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size: 15px;line-height: 150%;text-align: left;">
-                                <h1 style="display: block;font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size: 40px;font-style: normal;font-weight: bold;line-height: 125%;letter-spacing: -1px;margin: 0;text-align: left;color: #606060 !important;"><?php the_title(); ?></h1>
+                                <h1 style="display: block;font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size: 40px;font-style: normal;font-weight: bold;line-height: 125%;letter-spacing: -1px;margin: 0;text-align: left;color: #606060 !important;">Issue #<?php the_field('issue_number'); ?>: <?php the_title(); ?></h1>
                                 <em><?php the_date('l j F Y'); ?></em><br>
+                                <div style="margin:20px 0;font-family:monospace;"><?php the_field('tagline'); ?></div>
                                 <?php the_content(); ?>
                             </td>
                         </tr>
--- themes/mcrcoderdojo/content-post.php
+++ themes/mcrcoderdojo/content-post.php
@@ -1,62 +1,63 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php twentyfourteen_post_thumbnail(); ?>
+    <?php twentyfourteen_post_thumbnail(); ?>
 
-	<header class="entry-header">
-		<div class="entry-meta">
-			<?php $categories = get_the_category(); ?>
-			<span class="cat-links">
-			<?php foreach ($categories as $category): ?>
-				<a href="/<?php echo $category->slug; ?>/"><?php echo $category->name; ?></a>
-			<?php endforeach; ?>
-			</span>
-		</div>
+    <header class="entry-header">
+        <div class="entry-meta">
+            <?php $categories = get_the_category(); ?>
+            <span class="cat-links">
+            <?php foreach ($categories as $category): ?>
+                <a href="/<?php echo $category->slug; ?>/"><?php echo $category->name; ?></a>
+            <?php endforeach; ?>
+            </span>
+        </div>
 
-		<?php
+        <?php if (is_single()):
+            if (get_field('issue_number')): ?>
+                <h1 class="entry-title">Post #<?php the_field('issue_number') ?>: <?php the_title(); ?></h1>
+            <?php else: ?>
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php endif;
+            else: ?>
+            <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <?php endif; ?>
 
-			if (is_single()) {
-				the_title('<h1 class="entry-title">', '</h1>');
-			}
-			else {
-				the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-			}
-		?>
+        <div class="entry-meta">
+            <?php twentyfourteen_posted_on(); ?>
+        </div><!-- .entry-meta -->
+    </header><!-- .entry-header -->
 
-		<div class="entry-meta">
-			<?php twentyfourteen_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+    <?php if (is_search()): ?>
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div><!-- .entry-summary -->
+    <?php else: ?>
+    <div class="entry-content">
+        <div style="margin-bottom: 20px;font-family:monospace;"><?php the_field('tagline'); ?></div>
+        <?php
+            the_content();
+            if (is_singular('post') && get_field('links')): ?>
+                <h2>Links</h2>
+                    <ul>
+                <?php while (has_sub_field('links')): ?>
+                        <li>
+                            <a href="<?php the_sub_field('url'); ?>" target="_blank">
+                                <?php the_sub_field('title'); ?>
+                            </a><br />
+                            <?php the_sub_field('summary'); ?>
+                        </li>
+                <?php endwhile; ?>
+                </ul>
+            <?php endif;
 
-	<?php if (is_search()): ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else: ?>
-	<div class="entry-content">
-		<?php
-			the_content();
-			if (is_singular('post') && get_field('links')): ?>
-				<h2>Links</h2>
-					<ul>
-				<?php while (has_sub_field('links')): ?>
-						<li>
-							<a href="<?php the_sub_field('url'); ?>" target="_blank">
-								<?php the_sub_field('title'); ?>
-							</a><br />
-							<?php the_sub_field('summary'); ?>
-						</li>
-				<?php endwhile; ?>
-				</ul>
-			<?php endif;
+            wp_link_pages(array(
+                'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'twentyfourteen') . '</span>',
+                'after'       => '</div>',
+                'link_before' => '<span>',
+                'link_after'  => '</span>',
+            ));
+        ?>
+    </div><!-- .entry-content -->
+    <?php endif; ?>
 
-			wp_link_pages(array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'twentyfourteen') . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-			));
-		?>
-	</div><!-- .entry-content -->
-	<?php endif; ?>
-
-	<?php the_tags('<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>'); ?>
+    <?php the_tags('<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>'); ?>
 </article><!-- #post-## -->
