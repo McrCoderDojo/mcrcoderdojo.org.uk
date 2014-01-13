367b3d2b6d3bb2305dd046eeb5e88393570e76d0
Ben Nuttall <ben@bennuttall.com>
2014-01-13 00:05:37 +0000

    Add eventbrite embed code to future events (fix #13) and show date/time/venue info for past events

--- themes/mcrcoderdojo/content-event.php
+++ themes/mcrcoderdojo/content-event.php
@@ -15,37 +15,55 @@
 
             $venue = mcd_get_venue_from_array(get_field('venue'));
          ?>
-    </header><!-- .entry-header -->
+    </header>
 
     <?php if (is_search()): ?>
     <div class="entry-summary">
         <?php the_excerpt(); ?>
-    </div><!-- .entry-summary -->
+    </div>
     <?php else: ?>
     <div class="entry-content">
-        <?php
+        <div class="col">
+            <h2>Date &amp; Time</h2>
+            <?php echo $date->format(get_option('date_format')); ?><br />
+            <?php the_field('time'); ?>
+        </div>
 
-            if ($date > $today):
-                the_content();
-        ?>
+        <div class="col">
+            <h2>Venue</h2>
+            <strong><?php echo get_the_title($venue); ?></strong><br />
+            <?php the_field('address', $venue); ?><br />
+            <?php the_field('post_code', $venue); ?>
+        </div>
 
-                <h2>Date &amp; Time</h2>
-                <?php echo $date->format(get_option('date_format')); ?><br />
-                <?php the_field('time'); ?>
+        <?php if ($date > $today): ?>
 
-                <h2>Venue</h2>
-                <strong><?php echo get_the_title($venue); ?></strong><br />
-                <?php the_field('address', $venue); ?><br />
-                <?php the_field('post_code', $venue); ?>
+                <h2>Tickets</h2>
+                <?php
+                    $pattern = "/<iframe(.*?)<\/iframe>/";
+                    if (preg_match($pattern, get_field('eventbrite_embed_code'), $matches)) {
+                        echo $matches[0];
+                    }
+                    else {
+                        the_field('eventbrite_embed_code');
+                    }
 
-                <h3>Parking</h3>
-                <?php the_field('parking', $venue); ?>
+                ?>
 
-                <h3>Map</h3>
-                <?php mcd_google_map(get_field('post_code', $venue));
+                <h2>Map</h2>
+                <?php mcd_google_map(get_field('post_code', $venue)); ?>
 
-            else:
-                the_field('after_event_content');
+                <h2>Parking</h2>
+                <?php the_field('parking', $venue);
+
+                if ($post->post_content != ''): ?>
+                    <h2>More Info</h2>
+                    <?php the_content();
+                endif;
+
+            elseif (get_field('after_event_content')): ?>
+                <h2>Summary</h2>
+                <?php the_field('after_event_content');
             endif;
 
             wp_link_pages(array(
@@ -55,8 +73,8 @@
                 'link_after'  => '</span>',
             ));
         ?>
-    </div><!-- .entry-content -->
-    <?php endif; ?>
+    </div>
+    <?php endif;
 
-    <?php the_tags('<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>'); ?>
-</article><!-- #post-## -->
+    the_tags('<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>'); ?>
+</article>
